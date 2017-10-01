d12bf132f2fb16ab47318dc1679eb28a0d23fa1d
Alexander Tereshkov <alexander.tereshkov@exposit.com>
2017-10-01 18:07:07 +0300

    Initial commit

---
+++ DiberClient/ThirdViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ThirdViewController.swift
+//  DiberClient
+//
+//  Created by Alexander Tereshkov on 10/1/17.
+//  Copyright © 2017 Alexander Tereshkov. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
