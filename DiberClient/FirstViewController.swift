287552f42ed14c89f2ff441e3c9a9f20415300b1
Alexander Tereshkov <alexander.tereshkov@exposit.com>
2017-10-01 17:21:02 +0300

    Initial Commit

---
+++ DiberClient/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DiberClient
+//
+//  Created by Alexander Tereshkov on 10/1/17.
+//  Copyright © 2017 Alexander Tereshkov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DiberClient/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DiberClient
+//
+//  Created by Alexander Tereshkov on 10/1/17.
+//  Copyright © 2017 Alexander Tereshkov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
